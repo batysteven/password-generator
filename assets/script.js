bbf53f93cb86ebcdbd506a9d30436968ac4610db
Steven Baty <baty_steven@yahoo.com>
2020-11-16 03:53:22 -0700

    fixed randomly picking from array, used console.log to show

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,17 @@
 // Assignment code here
 //arrays for password
-var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var upperCaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const upperCaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+const specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 //randomly pick from options based on users response
 function randomPick() {
   var pick = Math.floor(Math.random() * 4);
-
+  
   if ((pick === 0) && (i === true)) {
     randomLowerCase();
   }
@@ -31,37 +31,35 @@ function randomPick() {
 
 //randomly pick from array
 function randomLowerCase() {
-  for (var i = 0; i < lowerCaseLetters.length; i++) {
-    Math.floor(Math.random() * lowerCaseLetters.length);
-    localStorage.setItem("password", lowerCaseLetters[i]);
-  }
+  const i = Math.floor(Math.random() * lowerCaseLetters.length);
+  localStorage.setItem("password", (i, lowerCaseLetters[i]));
 }
 
 function randomUpperCase() {
-  for (var j = 0; j < upperCaseLetter.length; j++) {
-    Math.floor(Math.random() * upperCaseLetter.length);
-    localStorage.setItem("password", upperCaseLetter[j]);
-  }
+  const j = Math.floor(Math.random() * upperCaseLetter.length);
+  localStorage.setItem("password", (j, upperCaseLetter[j]));
 }
 
 function randomNumber() {
-  for (var k = 0; k < numbers.length; k++) {
-    Math.floor(Math.random() * numbers.length);
-    localStorage.setItem("password", numbers[k]);
-  }
+  const k = Math.floor(Math.random() * numbers.length);
+  localStorage.setItem("password", (k, numbers[k]));
 }
 
 function randomSpecialChar() {
-  for (var p = 0; p < specialChar.length; p++ ) {
-    Math.floor(Math.random() * specialChar.length);
-    localStorage.setItem("password", specialChar[p]);
-  }
+  const p = Math.floor(Math.random() * specialChar.length)
+  localStorage.setItem("password", (p, specialChar[p]));
 }
 
 //ask questions about password and generate
 function generatePassword() {
   var passwordLength = window.prompt(
     "How long would you like your password to be between 8 - 128 characters long??");
+
+    passwordLength = parseInt(passwordLength);
+    if (8 > passwordLength > 128 || passwordLength === "" || passwordLength === null) {
+      window.alert("Not a valid option");
+      generatePassword();
+    }
 
     var lowerCaseConfirm = window.confirm("Would you like lower case letters?");
     if (lowerCaseConfirm) {
@@ -95,18 +93,9 @@ function generatePassword() {
       p = false;
     }
 
-    if (8 < passwordLength < 128) {
-      passwordLength = parseInt(passwordLength);
-
-      for (var q = 0; q < passwordLength; q++) {
-        randomPick();
-      }
+    for (var q = 0; q < passwordLength; q++) {
+      randomPick();
     }
-    if (8 > passwordLength > 128) {
-      window.alert("Not a valid option");
-      generatePassword();
-    }
-
 }
 
 // Get references to the #generate element
@@ -115,8 +104,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  var passwordText = document.querySelectorAll("#password");
   passwordText.value = password;
 
 }
