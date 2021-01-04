f852806abe0530a19dc9d3793f078f1e527210ca
Steven Baty <baty_steven@yahoo.com>
2021-01-04 15:27:24 -0700

    clear prev generated password if user clicks multiple times

--- assets/script.js
+++ assets/script.js
@@ -121,6 +121,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  $("password").empty();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
